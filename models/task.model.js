2d8b96cc2ab55d5ba76ddc76645290783b4c438b
Palak Tyagi <palaktyagi29@gmail.com>
2019-09-07 15:51:24 -0400

    Sequelize IC -submission for deadline

--- models/task.model.js
+++ models/task.model.js
@@ -1,13 +1,13 @@
-'use strict';
+"use strict";
 
-const db = require('./database');
-const Sequelize = require('sequelize');
+const db = require("./database");
+const Sequelize = require("sequelize");
 
 // Make sure you have `postgres` running!
 
 //---------VVVV---------  your code below  ---------VVV----------
 
-const Task = db.define('Task', {
+const Task = db.define("Task", {
   name: {
     type: Sequelize.STRING,
     allowNull: false,
@@ -22,12 +22,67 @@ const Task = db.define('Task', {
   due: Sequelize.DATE
 });
 
+Task.belongsTo(Task, { as: "parent" });
 
+Task.clearCompleted = function() {
+  this.destroy({
+    where: { complete: true }
+  });
+};
 
-Task.belongsTo(Task, {as: 'parent'});
+Task.completeAll = function() {
+  return this.update(
+    {
+      complete: true
+    },
+    { where: { complete: false } }
+  );
+};
 
+Task.prototype.getTimeRemaining = function() {
+  // const rDiff = this.due;
+  // if (!rDiff) {
+  //   return Infinity;
+  // } else {
+  //   return rDiff - new Date();
+  // }
+  //Ternary operator
+  return !this.due ? Infinity : this.due - new Date();
+};
 
+Task.prototype.isOverdue = function() {
+  // const todayDate = new Date();
+  // if (this.due > todayDate || (this.due < todayDate && this.complete)) {
+  //   return false;
+  // } else {
+  //   return true;
+  // }
+
+  //Ternary Operator
+  return !(this.due > new Date() || (this.due < new Date() && this.complete));
+};
+
+Task.prototype.addChild = function(child) {
+  child.parentId = this.id;
+  return Task.create(child);
+};
+
+Task.prototype.getChildren = function() {
+  return Task.findAll({
+    where: {
+      parentId: this.id
+    }
+  });
+};
+
+Task.prototype.getSiblings = function() {
+  return Task.findAll({
+    where: {
+      parentId: this.parentId,
+      id: { $ne: this.id }
+    }
+  });
+};
 //---------^^^---------  your code above  ---------^^^----------
 
 module.exports = Task;
-
--- test/task.model.spec.js
+++ test/task.model.spec.js
@@ -1,11 +1,11 @@
 /* eslint-disable no-unused-expressions */
 /* eslint-env mocha, chai */
-'use strict';
+"use strict";
 
-const helper = require('./helper');
-const expect = require('chai').expect;
-const db = require('../models/database');
-const Task = require('../models/task.model');
+const helper = require("./helper");
+const expect = require("chai").expect;
+const db = require("../models/database");
+const Task = require("../models/task.model");
 
 /**
  * Start here
@@ -14,67 +14,76 @@ const Task = require('../models/task.model');
  *
  */
 
-describe('Task', function () {
-
+describe("Task", function() {
   // clear the database before all tests
   before(() => {
-    return db.sync({force: true});
+    return db.sync({ force: true });
   });
 
   // erase all tasks after each spec
   afterEach(() => {
-    return db.sync({force: true});
+    return db.sync({ force: true });
   });
 
-  describe('Class methods', function () {
-
+  describe("Class methods", function() {
     beforeEach(async () => {
       await Promise.all([
-        Task.create({ name: 't1', due: helper.dates.tomorrow() }),
-        Task.create({ name: 't2', due: helper.dates.tomorrow(), complete: true }),
-        Task.create({ name: 't3', due: helper.dates.yesterday() }),
-        Task.create({ name: 't4', due: helper.dates.yesterday(), complete: true })
+        Task.create({ name: "t1", due: helper.dates.tomorrow() }),
+        Task.create({
+          name: "t2",
+          due: helper.dates.tomorrow(),
+          complete: true
+        }),
+        Task.create({ name: "t3", due: helper.dates.yesterday() }),
+        Task.create({
+          name: "t4",
+          due: helper.dates.yesterday(),
+          complete: true
+        })
       ]);
     });
 
-    describe('clearCompleted', function () {
-      xit('removes all completed tasks from the database', async function () {
+    describe("clearCompleted", function() {
+      it("removes all completed tasks from the database", async function() {
         await Task.clearCompleted();
 
-        const completedTasks = await Task.findAll({ where: { complete: true } });
-        const incompleteTasks = await Task.findAll({ where: { complete: false } });
+        const completedTasks = await Task.findAll({
+          where: { complete: true }
+        });
+        const incompleteTasks = await Task.findAll({
+          where: { complete: false }
+        });
 
         expect(completedTasks).to.have.length(0);
         expect(incompleteTasks).to.have.length(2);
       });
     });
 
-    describe('completeAll', function () {
-
-      xit('marks all incomplete tasks as completed', async function () {
+    describe("completeAll", function() {
+      it("marks all incomplete tasks as completed", async function() {
         await Task.completeAll();
 
-        const completedTasks = await Task.findAll({ where: { complete: true } });
-        const incompleteTasks = await Task.findAll({ where: { complete: false } });
+        const completedTasks = await Task.findAll({
+          where: { complete: true }
+        });
+        const incompleteTasks = await Task.findAll({
+          where: { complete: false }
+        });
 
         expect(completedTasks).to.have.length(4);
         expect(incompleteTasks).to.have.length(0);
       });
-
     });
-
   });
 
-  describe('Instance methods', function () {
-
-    describe('getTimeRemaining', function () {
-
-      xit('returns the Infinity value if task has no due date', function () {
+  describe("Instance methods", function() {
+    describe("getTimeRemaining", function() {
+      it("returns the Infinity value if task has no due date", function() {
         const task = Task.build();
         expect(task.getTimeRemaining()).to.equal(Infinity);
       });
 
-      xit('returns the difference between due date and now', function () {
+      it("returns the difference between due date and now", function() {
         const oneDay = 24 * 60 * 60 * 1000; // one day in milliseconds
 
         // create a task due one day from this point in time
@@ -84,19 +93,17 @@ describe('Task', function () {
 
         expect(task.getTimeRemaining()).to.be.closeTo(oneDay, 10); // within 10 ms
       });
-
     });
 
-    describe('isOverdue', function () {
-
-      xit('is overdue if the due date is in the past', function () {
+    describe("isOverdue", function() {
+      it("is overdue if the due date is in the past", function() {
         const task = Task.build({
           due: helper.dates.yesterday()
         });
         expect(task.isOverdue()).to.be.true;
       });
 
-      xit('is not overdue if the due date is in the past but complete is true', function () {
+      it("is not overdue if the due date is in the past but complete is true", function() {
         const task = Task.build({
           due: helper.dates.yesterday(),
           complete: true
@@ -104,7 +111,7 @@ describe('Task', function () {
         expect(task.isOverdue()).to.be.false;
       });
 
-      xit('is not overdue if the due date is in the future', function () {
+      it("is not overdue if the due date is in the future", function() {
         const task = Task.build({
           due: helper.dates.tomorrow()
         });
@@ -112,46 +119,38 @@ describe('Task', function () {
       });
     });
 
-    describe('addChild', function () {
-
-      xit('should return a promise for the new child', async function () {
-        const parentTask = await Task.create({ name: 'parent task' });
-        const childTask = await parentTask.addChild({ name: 'child task' });
-        expect(childTask.name).to.equal('child task');
+    describe("addChild", function() {
+      it("should return a promise for the new child", async function() {
+        const parentTask = await Task.create({ name: "parent task" });
+        const childTask = await parentTask.addChild({ name: "child task" });
+        expect(childTask.name).to.equal("child task");
         expect(childTask.parentId).to.equal(parentTask.id);
       });
-
     });
 
-    describe('getChildren', function () {
-
-      xit('should return a promise for an array of the task\'s children', async function () {
-        const parentTask = await Task.create({ name: 'parent task' });
-        await parentTask.addChild({ name: 'child task' });
-        await parentTask.addChild({ name: 'child task 2' });
+    describe("getChildren", function() {
+      it("should return a promise for an array of the task's children", async function() {
+        const parentTask = await Task.create({ name: "parent task" });
+        await parentTask.addChild({ name: "child task" });
+        await parentTask.addChild({ name: "child task 2" });
 
         const children = await parentTask.getChildren();
 
         expect(children).to.have.length(2);
-        expect(children[0].name).to.equal('child task');
+        expect(children[0].name).to.equal("child task");
       });
-
     });
 
-    describe('getSiblings', function () {
-
-      xit('returns a promise for an array of siblings', async function () {
-        const parentTask = await Task.create({ name: 'parent task' });
-        const firstChild = await parentTask.addChild({ name: 'first child' });
-        const secondChild = await parentTask.addChild({ name: 'second child' });
+    describe("getSiblings", function() {
+      it("returns a promise for an array of siblings", async function() {
+        const parentTask = await Task.create({ name: "parent task" });
+        const firstChild = await parentTask.addChild({ name: "first child" });
+        const secondChild = await parentTask.addChild({ name: "second child" });
 
         const siblings = await firstChild.getSiblings();
         expect(siblings).to.have.length(1);
         expect(siblings[0].id).to.equal(secondChild.id);
       });
-
     });
-
   });
-
 });
